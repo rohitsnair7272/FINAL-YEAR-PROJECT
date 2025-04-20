c8a9ef34d939bbc0fcebbf33a2ec53aee10f4fda
Rohit Nair <rohitsnair2004@gmail.com>
2025-04-20 12:13:09 +0530

    changes made

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,6 +1,10 @@
 # mongo_utils.py
 from pymongo import MongoClient
 import os
+from dotenv import load_dotenv
+
+# ✅ Load environment variables
+load_dotenv()
 
 # Replace with your actual MongoDB URI
 MONGO_URI = os.getenv("MONGO_URI")
