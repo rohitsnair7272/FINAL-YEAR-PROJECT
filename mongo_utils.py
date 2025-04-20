ddd12d80bcc2e7964857da3204535eb217d3ff0a
Rohit Nair <rohitsnair2004@gmail.com>
2025-04-20 12:10:32 +0530

    changes made

--- mongo_utils.py
+++ mongo_utils.py
@@ -1,8 +1,9 @@
 # mongo_utils.py
 from pymongo import MongoClient
+import os
 
 # Replace with your actual MongoDB URI
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["ShopkeepersDB"]
 shopkeepers_col = db["shopkeepers"]
