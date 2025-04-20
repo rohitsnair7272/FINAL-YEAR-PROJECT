846e89a325ce52abb3a5b9b3de3d918eb6c1a50d
Rohit Nair <rohitsnair2004@gmail.com>
2025-04-21 00:27:56 +0530

    changes made

--- server.py
+++ server.py
@@ -253,4 +253,4 @@ async def delete_product(name: str = Form(...)):
 # ✅ Run server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
